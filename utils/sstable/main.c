fb74759b7d112839be1dac6fb3d04a9e53950b99
Bob Poekert <bob@poekert.com>
2018-05-25 16:44:58 -0700

    add mtbl

---
+++ utils/sstable/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include "shmemq.h"
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
